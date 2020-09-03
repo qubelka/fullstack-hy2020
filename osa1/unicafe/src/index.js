f319e2740baa96ec38fbd49a9f88f51bfcab89a6
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-09-03 13:29:35 +0300

    1.8

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,19 +1,11 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-
+const Statistics = ({good, neutral, bad}) => {
     const all = good + neutral + bad
 
     return (
-        <div>
-            <p style={{fontSize:"26px", fontWeight:"bold"}}>give feedback</p>
-            <button type="button" onClick={() => setGood(good+1)}>good</button>
-            <button type="button" onClick={() => setNeutral(neutral+1)}>neutral</button>
-            <button type="button" onClick={() => setBad(bad+1)}>bad</button><br/>
+        <>
             <p style={{fontSize:"26px", fontWeight:"bold"}}>statistics</p>
             <p>
                 good {good}<br/>
@@ -23,6 +15,22 @@ const App = () => {
                 average {(good + bad*(-1))/all} <br/>
                 positive {(good/all)*100} % <br/>
             </p>
+        </>
+    )
+}
+
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    return (
+        <div>
+            <p style={{fontSize:"26px", fontWeight:"bold"}}>give feedback</p>
+            <button type="button" onClick={() => setGood(good+1)}>good</button>
+            <button type="button" onClick={() => setNeutral(neutral+1)}>neutral</button>
+            <button type="button" onClick={() => setBad(bad+1)}>bad</button><br/>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
         </div>
     )
 }
