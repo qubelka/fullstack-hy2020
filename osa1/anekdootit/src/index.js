414cf00ff86fc491d980ff09015a7033a349cc2e
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-09-03 16:21:30 +0300

    1.12

---
+++ osa1/anekdootit/src/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({handleClick}) =>
+  <button onClick={handleClick}>next anecdote</button>
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  const handleClick = () => {
+    let randomNumber = Math.floor(Math.random()*props.anecdotes.length)
+    setSelected(randomNumber)
+  }
+
+  return (
+    <div>
+      <p>{props.anecdotes[selected]}</p>
+      <Button handleClick={handleClick}/>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
