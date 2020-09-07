58f4c0dab11871c8022c28c181ba8fb8d1e132dc
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-09-07 14:18:57 +0300

    2.5

---
+++ osa2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Header = ({course}) =>
+    <h2>{course.name}</h2>
+
+
+const Part = ({part}) =>
+    <p>
+        {part.name} {part.exercises}
+    </p>
+
+
+const Content = ({course}) => {
+    return (
+        <div>
+            {course.parts.map(part => <Part key={part.id} part={part}/>)}
+        </div>
+    )
+}
+
+const Total = ({course}) => {
+    const initialValue = 0
+    const sum = course.parts.reduce((p1, p2) => {
+        return p1 + p2.exercises
+    }, initialValue)
+
+    return (
+        <p style={{fontWeight:"bold"}}>total of {sum} exercises</p>
+    )
+}
+
+const Course = ({course}) => {
+    return (
+        <>
+            <Header course={course} />
+            <Content course={course} />
+            <Total course={course} />
+        </>
+    )
+}
+
+export default Course
