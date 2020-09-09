15f82ddfee3cc74daa49ee6f88082241392c7068
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-09-09 18:39:35 +0300

    2.14

--- osa2/maiden_tiedot/src/components/CountryFullInfo.js
+++ osa2/maiden_tiedot/src/components/CountryFullInfo.js
@@ -1,7 +1,21 @@
-import Languages from "./Languages";
-import React from "react";
+import Languages from "./Languages"
+import Weather from "./Weather";
+import React, {useState, useEffect} from "react"
+import axios from 'axios'
 
 const CountryFullInfo = ({country}) => {
+    const [weather, setWeather] = useState('')
+
+    useEffect(() => {
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${country.name}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+        axios
+            .get(url)
+            .then(response => {
+                setWeather(response.data)
+            })
+    }, [country])
+
+
     return (
         <>
             <h1>{country.name}</h1>
@@ -12,6 +26,8 @@ const CountryFullInfo = ({country}) => {
             <h2>languages</h2>
             <Languages country={country}/>
             <img height={'80'} width={'110'} src={country.flag} alt={`Flag of ${country.name}`}/>
+            <h2>Weather in {country.name}</h2>
+            {weather ? <Weather weather={weather}/> : ''}
         </>
     )
 }
