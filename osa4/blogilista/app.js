ffe776b1632811a816bd5acba727343cca29b074
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-10-05 14:21:43 +0300

    4.1-4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+const mongoUrl = config.MONGODB_URI
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
