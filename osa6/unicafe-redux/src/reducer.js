0437f3d61ad7f73557bb6037f48eec9f9fe9894c
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-11-18 09:19:44 +0200

    6.1-6.2

---
+++ osa6/unicafe-redux/src/reducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+      return Object.assign({}, state, {good: state.good+1})
+    case 'OK':
+      return Object.assign({}, state, {ok: state.ok+1})
+    case 'BAD':
+      return Object.assign({}, state, {bad: state.bad+1})
+    case 'ZERO':
+      return initialState
+    default: return state
+  }
+}
+
+export default counterReducer
