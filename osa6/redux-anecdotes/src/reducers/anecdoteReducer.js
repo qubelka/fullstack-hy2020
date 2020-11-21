1669eeefcbb8f21612e6ecdadfc23ed686759dae
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-11-21 13:39:08 +0200

    6.13-6.14

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,15 +1,4 @@
-import { VOTE, NEW_ANECDOTE } from '../constants/action-types'
-
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
+import { VOTE, NEW_ANECDOTE, INIT_ANECDOTES } from '../constants/action-types'
 
 export const vote = id => {
   return {
@@ -18,29 +7,24 @@ export const vote = id => {
   }
 }
 
-export const createAnecdote = content => {
+export const createAnecdote = data => {
   return {
     type: NEW_ANECDOTE,
-    data: {
-      content,
-      id: getId(),
-      votes: 0
-    }
+    data
   }
 }
 
-const asObject = (anecdote) => {
+export const initializeAnecdotes = anecdotes => {
   return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
+    type: INIT_ANECDOTES,
+    data: anecdotes
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   switch (action.type) {
+    case INIT_ANECDOTES:
+      return action.data
     case VOTE:
       const id = action.data.id
       const anecdote = state.find(anecdote => anecdote.id === id)
